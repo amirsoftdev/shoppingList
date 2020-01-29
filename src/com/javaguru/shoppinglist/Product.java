f568885e783caaaf83990776740d5a6ee8ec7eff
amirkerimov <amirkerimovgym@gmail.com>
2020-01-29 15:48:38 +0200

    Created validation, created option to delete and change the product.

--- src/com/javaguru/shoppinglist/Product.java
+++ src/com/javaguru/shoppinglist/Product.java
@@ -1,12 +1,31 @@
 package com.javaguru.shoppinglist;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+
 
 public class Product {
 
     private Long id;
     private String name;
     private BigDecimal price;
+    private String category;
+    private BigDecimal discount;
+    private String description;
+
+    public void validate() throws UserValidationException {
+        if (price.compareTo(BigDecimal.ZERO) < 0) {
+            throw new UserValidationException("Price cannot be smaller that 0");
+
+        } else if (discount.compareTo(BigDecimal.valueOf(0)) < 0 || discount.compareTo(BigDecimal.valueOf(100)) > 0) {
+            throw new UserValidationException("Discount cannot be smaller that 0 % or bigger that 100 % ");
+
+
+        } else if (name == null || name.length() < 3 || name.length() > 32) {
+            throw new UserValidationException("Price name cannot be smaller that 3 and bigger that 32");
+        }
+    }
+
 
     public Long getId() {
         return id;
@@ -31,5 +50,6 @@ public class Product {
     public void setPrice(BigDecimal price) {
         this.price = price;
     }
+
 
 }
--- src/com/javaguru/shoppinglist/ShoppingListApplication.java
+++ src/com/javaguru/shoppinglist/ShoppingListApplication.java
@@ -16,6 +16,8 @@ class ShoppingListApplication {
                 System.out.println("1. Create product");
                 System.out.println("2. Find product by id");
                 System.out.println("3. Exit");
+                System.out.println("4. Delete product");
+                System.out.println("5. Change product");
                 Integer userInput = Integer.valueOf(scanner.nextLine());
                 switch (userInput) {
                     case 1:
@@ -37,6 +39,36 @@ class ShoppingListApplication {
                         System.out.println(findProductResult);
                     case 3:
                         return;
+                    case 4:
+                        System.out.println("If you want to delete product , please Enter product id: ");
+                        long idToDelete = scanner.nextLong();
+                        for (Long iterLong : productRepository.keySet()) {
+
+                            if (productRepository.containsKey(idToDelete)) {
+                                idToDelete = productIdSequence;
+                            } else {
+                                System.out.println("Wrong key, please try again");
+                                break;
+                            }
+                        }
+                        productRepository.remove(idToDelete);
+
+                    case 5:
+                        Product productForReplace = new Product();
+                        System.out.println("If you want to update product , please Enter product id: ");
+                        long updateProduct = scanner.nextLong();
+                        for (Long iterLong : productRepository.keySet()) {
+
+                            if (productRepository.containsKey(updateProduct)) {
+                                updateProduct = productIdSequence;
+                            } else {
+                                System.out.println("Wrong key, please try again");
+                                break;
+                            }
+                        }
+                        productRepository.put(productIdSequence, productForReplace);
+
+
                 }
             } catch (Exception e) {
                 System.out.println("Error! Please try again.");
